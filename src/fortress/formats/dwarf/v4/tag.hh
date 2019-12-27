6705212cdb0a033354abf38414067939a526421c
Aki Van Ness <aki@lethalbit.net>
2019-12-27 18:21:54 -0500

    Fixed a typo

--- src/fortress/formats/dwarf/v4/tag.hh
+++ src/fortress/formats/dwarf/v4/tag.hh
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-/* v4/tag.hh - DWARF3 Tag definitions and helpers */
+/* v4/tag.hh - DWARF4 Tag definitions and helpers */
 #pragma once
 #if !defined(_LIBFORTRESS_FORMATS_DWARF_V4_TAG_HH_)
 #define _LIBFORTRESS_FORMATS_DWARF_V4_TAG_HH_
