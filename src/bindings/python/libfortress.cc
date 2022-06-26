998bd7c6ce921d8d6f13b046eb2a4cac33b3d146
Aki Van Ness <aki@lethalbit.net>
2022-06-25 20:36:34 -0400

    src: bindings: python: Updated python bindings to ensure it has the proper submodules

--- src/bindings/python/libfortress.cc
+++ src/bindings/python/libfortress.cc
@@ -11,4 +11,13 @@ PYBIND11_MODULE(libfortress, m) {
 	m.doc() = "A library for parsing and generating various debug file formats.";
 	m.attr("__version__") = Fortress::Config::version;
 
+	[[maybe_unused]]
+	auto dwarf = m.def_submodule("dwarf", "Fortress module for parsing and generating DWARF debug info");
+	[[maybe_unused]]
+	auto pdb = m.def_submodule("pdb", "Fortress module for parsing and generating PDB debug info");
+	[[maybe_unused]]
+	auto stabs = m.def_submodule("stabs", "Fortress module for parsing and generating STABS debug info");
+	[[maybe_unused]]
+	auto vms = m.def_submodule("vms", "Fortress module for parsing and generating VMS debug info");
+
 }
